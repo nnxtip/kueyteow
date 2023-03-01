c0fd786320940867297938d66d098ebf01a4baea
Pasitcodingclass <c763480@gmail.com>
2023-03-01 14:34:47 +0700

    even odd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+    static void Main(string[] Args) {
+        int odd = 0;
+
+        while(true) {
+            int x = int.Parse(Console.ReadLine());
+        
+
+        if (x % 2 == 0) {
+            Console.WriteLine("even");
+            odd = 0;
+            
+        } else {
+            if (odd ==1) {
+                break;
+            } else {
+                Console.WriteLine("-1");
+                odd = 1;
+            }
+        }
+    }
+}
+}
+        
+            
+            
+                
+            
+
+        
+
+    
+
